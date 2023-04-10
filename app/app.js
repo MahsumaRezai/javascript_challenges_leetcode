175f1bc5bee77c8113485be4b82d4593e4ef254d
Masoumeh Rezai <you@example.com>
2023-04-11 00:47:44 +0430

    15 challenges in javascript leetcode

--- app/app.js
+++ app/app.js
@@ -1,18 +1,6 @@
 
-// 3
-// 12,3,3,4,4
-function arrlist() {
-    let arr = [1, 2, 3, 4];
-    for (let i = 0; i < arr.length; i++) {
-        if (arr.length - 1 > arr.length) {
-            return false;
-        }
-        else if (arr.length - 1 < arr.length) {
-            return true
-        }
-    }
-}
-arrlist();
+
+
 
 
 
