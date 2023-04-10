94493a415f5a78a9b55b5ccb551670c6438fe842
Masoumeh Rezai <you@example.com>
2023-04-10 22:32:57 +0430

    challenge 8

--- app/app.js
+++ app/app.js
@@ -1,1 +1,13 @@
-
+// 8
+function nodel(s) {
+    let arr = [1, 2, 3, 4];
+    for (let i = 0; i < arr.length; i++) {
+        if (s == arr) {
+            return true;
+        }
+        else if (s != arr) {
+            return false;
+        }
+    }
+}
+nodel();
