55330486e963273d06773aa6054dd1bc2d17b52d
Masoumeh Rezai <you@example.com>
2023-04-12 00:38:29 +0430

    challenge 17

---
+++ app/challenge17.js
@@ -0,0 +1,17 @@
+// 17
+function remov() {
+    let head;
+    let curr = head;
+    while (curr && curr.next) {
+
+        if (curr.val === curr.next.val) {
+            curr.next = curr.next.next;
+        }
+        else {
+            curr = curr.next;
+        }
+        return head;
+
+    }
+}
+remov()
