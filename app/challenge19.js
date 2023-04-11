ceab5f8136124fae4550d81a3d843c54d1d6f363
Masoumeh Rezai <you@example.com>
2023-04-12 00:47:14 +0430

    challenge 19

---
+++ app/challenge19.js
@@ -0,0 +1,12 @@
+
+function arrcheck(...arr2) {
+    let arr1 = [1, 2, 3];
+    if (arr1 == arr2) {
+        return true;
+    }
+    else if (arr1 != arr2) {
+        return false;
+    }
+
+}
+arrcheck();
