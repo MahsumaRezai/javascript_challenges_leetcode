6a93ffc49ab8722d3ff6788ae7e19c65c4a6d4a6
Masoumeh Rezai <you@example.com>
2023-04-12 00:41:46 +0430

    challenge 18

--- app/app.js
+++ app/app.js
@@ -1,3 +1,12 @@
+// root  list 
+function root() {
+    let arr = [1, 2, 3, 4, 5];
+    for (let i = 0; i < arr.length; i++) {
+        let arr2 = arr[0];
+        console.log(arr2)
+    }
+}
+root()
 
 
 
