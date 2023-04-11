499e12690db177576c3e3d454bc9606376e285f4
Masoumeh Rezai <you@example.com>
2023-04-12 00:54:45 +0430

    challenge 20

---
+++ app/challenge20.js
@@ -0,0 +1,11 @@
+// 
+function str(a, b) {
+    if (a == b) {
+        console.log("your massage is ture")
+    }
+    else if (a != b) {
+        console.log("your massage is not ture")
+    }
+
+}
+str();
