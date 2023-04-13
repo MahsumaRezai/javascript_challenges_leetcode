2bacfd09688285ccee54ef33ef059f049ca1edb4
Masoumeh Rezai <you@example.com>
2023-04-14 01:21:11 +0430

    challenge 30

---
+++ app/challenge29.js
@@ -0,0 +1,18 @@
+// 29 
+class name {
+    constructor(name, last) {
+        this.name = name;
+        this.last = last;
+
+    }
+    syhi() {
+        alert("Hello form the js challenges ");
+
+    }
+    sybo() {
+        alert("By By form ths js challenge challenges ")
+    }
+
+}
+let name2 = new name("Masoumeh", "Rezai");
+console.log(name2)
