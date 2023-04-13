dcf475080cfa5e239b54f2e756b185dcdbf343c7
Masoumeh Rezai <you@example.com>
2023-04-14 00:47:41 +0430

    challenge 22

--- app/app.js
+++ app/app.js
@@ -1,8 +1,13 @@
 
+//23 
 
+let arr = [1, 2, 3, 4, 5, 12];
+let arr2 = arr.reduce((a, b) => {
+    if (b > a) return ++b;
+    return ++a;
 
-
-
+})
+console.log(arr2);
 
 
 
---
+++ app/challenge22.js
@@ -0,0 +1,6 @@
+let srt = "Hello world";
+for (let i = 6; i < srt.length; i++) {
+    if (srt.lastIndexOf()) {
+        console.log(srt[i])
+    }
+}
