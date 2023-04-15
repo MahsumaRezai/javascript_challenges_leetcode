a2db309f640c7c7aa4fa9e27661dc6dc1072643b
Masoumeh Rezai <you@example.com>
2023-04-16 03:59:31 +0430

    challenge 31

--- app/app.js
+++ app/app.js
@@ -1,16 +1,12 @@
-// 30 
-function addlist() {
-    let arr = [1, 2, 3, 4];
-    let arr2 = [5, 6, 7, 8];
-    let arr3 = arr.concat(arr2);
-    console.log(arr3)
-    arr3.reduce((a, b) => {
-        let arr4 = b + a;
-        console.log(arr4)
+let prime = new Promise(function (resolve, reject) {
+    let text = "Hello";
+    if (text.length >= 10) {
+        alert("your text is ture");
+        return resolve;
+    }
+    else if (text.length <= 10) {
+        alert("your text is not ture");
+        return reject;
+    }
 
-    })
-
-
-}
-addlist()
-
+})
