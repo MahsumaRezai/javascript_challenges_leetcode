bd44fd9eb2d5cee81862500bd6b368d65d95191e
Masoumeh Rezai <you@example.com>
2023-04-16 04:07:30 +0430

    challenge 33

--- app/app.js
+++
@@ -1,9 +0,0 @@
-function text() {
-    let text1 = "Hello form the js code";
-    for (let i = 0; i < text1.length; i++) {
-        let text2 = text1.split(" ");
-        console.log(text2)
-    }
-
-}
-text();
---
+++ app/challenge32.js
@@ -0,0 +1,9 @@
+function text() {
+    let text1 = "Hello form the js code";
+    for (let i = 0; i < text1.length; i++) {
+        let text2 = text1.split(" ");
+        console.log(text2)
+    }
+
+}
+text();
