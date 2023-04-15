2dbd038ac11d9919feaa376f69daaf7df49f8434
Masoumeh Rezai <you@example.com>
2023-04-16 04:21:17 +0430

    challenge 35

--- app/app.js
+++ app/app.js
@@ -1,7 +1,5 @@
-function water(width) {
-    let need_water = (width * 30) / 280;
-    console.log(need_water);
-
-
+function add() {
+    let intfloat = 23.4;
+    console.log(parseInt(intfloat));
 }
-water();
+add()
