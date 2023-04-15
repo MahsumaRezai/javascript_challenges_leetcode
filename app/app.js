1d5996562610872d2657540832767bbfbb632147
Masoumeh Rezai <you@example.com>
2023-04-16 03:52:25 +0430

    challenge 30

--- app/app.js
+++ app/app.js
@@ -1,2 +1,16 @@
+// 30 
+function addlist() {
+    let arr = [1, 2, 3, 4];
+    let arr2 = [5, 6, 7, 8];
+    let arr3 = arr.concat(arr2);
+    console.log(arr3)
+    arr3.reduce((a, b) => {
+        let arr4 = b + a;
+        console.log(arr4)
 
+    })
+
+
+}
+addlist()
 
