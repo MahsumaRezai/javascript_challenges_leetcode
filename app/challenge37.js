b79d8c07efe618fa8c51ba5f2426bbaed18d4f9b
Masoumeh Rezai <you@example.com>
2023-04-16 22:28:21 +0430

    challenge 37

---
+++ app/challenge37.js
@@ -0,0 +1,15 @@
+function sum() {
+    let arr = [
+        [1, 2, 3, 4]
+        [5, 6, 7, 8]
+    ]
+    let arr2 = [
+        [1, 2, 3, 4]
+        [5, 6, 7, 8]
+    ]
+    let arr3 = arr.concat(arr2);
+    console.log(arr3)
+
+
+}
+sum()
