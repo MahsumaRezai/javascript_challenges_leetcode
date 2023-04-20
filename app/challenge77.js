4116d078f7b50f488d881f63e4fe7296984f595e
Masoumeh Rezai <you@example.com>
2023-04-21 02:29:36 +0430

    challenge 78

---
+++ app/challenge77.js
@@ -0,0 +1,14 @@
+function notvlaue() {
+    let arr = [1, 2, 3, 4];
+    for (let i = 0; i < arr.length; i++) {
+        if (arr.length - 1 == 0) {
+            let arr2 = arr.push("Hello form the js code");
+            console.log(arr2)
+
+        }
+        else {
+            console.log(arr)
+        }
+    }
+}
+notvlaue()
