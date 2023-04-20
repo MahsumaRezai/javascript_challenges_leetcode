8c082683ed9522801c3d345697370228ade9a64f
Masoumeh Rezai <you@example.com>
2023-04-21 02:01:35 +0430

    challenge 75

---
+++ app/challenge75.js
@@ -0,0 +1,11 @@
+function root() {
+    let arr = [1, 2, 3, 4, 5, 6];
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] = 0) {
+            let arr2 = null;
+            console.log(arr2)
+
+        }
+    }
+}
+root()
