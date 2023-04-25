f1f75d63c282f651d71f4febec103c56a9532981
Masoumeh Rezai <you@example.com>
2023-04-25 07:16:53 +0430

    challeneg 111

---
+++ app/challenge111.js
@@ -0,0 +1,13 @@
+function arr() {
+    let arr1 = [1, 2, 3, 4];
+    let arr2 = [5, 6, 7, 8];
+    for (let i = 0; i < arr1.length; i++) {
+        for (let j = 0; j < arr2.length; j++) {
+            if (arr1.length == arr2.length) {
+                console.log("hello")
+            }
+        }
+    }
+
+}
+arr();
