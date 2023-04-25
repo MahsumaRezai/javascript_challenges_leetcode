bb4628ad348526d5ec65000924da79f1f1eaa6b5
Masoumeh Rezai <you@example.com>
2023-04-25 07:20:11 +0430

    challenge 112

---
+++ app/challeneg112.js
@@ -0,0 +1,13 @@
+function cand() {
+    let arr = [1, 2, 3, 4, 5];
+    for (let i = 0; i < arr.length; i++) {
+        if (arr.length - 1 < arr.length) {
+            console.log(arr.length - 1)
+        }
+        else if (arr.length - 1 > arr.length) {
+            console.log("bay")
+        }
+    }
+
+}
+cand();
