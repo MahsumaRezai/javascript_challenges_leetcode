1c0951e479db945f96d214142814495dad6db29a
Masoumeh Rezai <you@example.com>
2023-04-26 23:58:41 +0430

    challenge 136

---
+++ app/challenge135.js
@@ -0,0 +1,8 @@
+
+let obj = {
+    f: "apple",
+    width: 20,
+}
+let res = obj.f = "banana";
+let res1 = obj.width = 10;
+console.log(res, res1)
---
+++ app/challenge136.js
@@ -0,0 +1,11 @@
+function arr(n) {
+    for (let i = 0; i < n; i++) {
+        if (n >= 10) {
+            let res = --n;
+            console.log(res)
+
+        }
+    }
+
+}
+arr();
