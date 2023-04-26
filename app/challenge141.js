af8125135cdb2fb64733d013ee6dd59f0d283f78
Masoumeh Rezai <you@example.com>
2023-04-27 00:26:14 +0430

    challenge 141

---
+++ app/challenge141.js
@@ -0,0 +1,9 @@
+function str() {
+    let s = ["Hello form the js code"];
+    for (let i = 0; i < s.length; i++) {
+        let res = s.sort();
+        let res1 = res.reverse();
+        console.log(res1)
+    }
+}
+str();
