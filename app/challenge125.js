37557efb7aad9a1c8699e6c4226a881666e1c977
Masoumeh Rezai <you@example.com>
2023-04-26 06:32:48 +0430

    challenge 126

---
+++ app/challenge125.js
@@ -0,0 +1,10 @@
+let obj = {
+    name: "jon",
+    department: "it",
+    salary: 20000,
+}
+let res = obj.name;
+let res1 = obj.department;
+let res3 = obj.salary;
+let r = res + " " + " " + res1 + " " + res3;
+console.log(r)
---
+++ app/challenge126.js
@@ -0,0 +1,9 @@
+let obj = {
+    name: "Mahsuam",
+    id: 96321
+}
+let obj2 = {
+    ...obj,
+    name1: "sara",
+}
+console.log(obj2)
