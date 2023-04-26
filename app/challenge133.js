322c0a5ef7273f75a2ef2fc2571d483820461598
Masoumeh Rezai <you@example.com>
2023-04-26 23:51:12 +0430

    challenge 133

---
+++ app/challenge133.js
@@ -0,0 +1,13 @@
+function arr() {
+    let arr1 = [[1, 2, 3]];
+    for (let i = 0; i < arr1.length; i++) {
+        if (arr1.length == 1) {
+            return true;
+        }
+        else if (arr1.length != 1) {
+            return false
+
+        }
+    }
+}
+arr();
