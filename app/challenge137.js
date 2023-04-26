92b77faefb8ebcba4edc4359dde5de55ed2eb849
Masoumeh Rezai <you@example.com>
2023-04-27 00:04:41 +0430

    challenge 137

---
+++ app/challenge137.js
@@ -0,0 +1,14 @@
+function s() {
+    let n = "Hello";
+    let m = "hi";
+    let res = n.startsWith("H");
+    let res1 = m.startsWith("h");
+    if (res = res1) {
+        console.log("Hello form the js code")
+    }
+    else if (res != res1) {
+        console.log("good bay js code")
+    }
+
+}
+s();
