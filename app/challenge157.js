07c211253042fb99a25cb14c347c7109ffcc60d7
Masoumeh Rezai <you@example.com>
2023-04-27 22:22:28 +0430

    challenge 157

---
+++ app/challenge157.js
@@ -0,0 +1,14 @@
+function arr() {
+    let arr1 = [1, 2];
+    let arr2 = arr1.pop();
+    let arr3 = arr1.push(arr2)
+    if (arr3 = arr2) {
+        return true;
+    }
+    else if (arr3 != arr2) {
+        return false
+    }
+
+}
+arr();
+
