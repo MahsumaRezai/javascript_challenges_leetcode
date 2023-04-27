4d4f69c41a9a1d8ed167da5c31bf1f572b33da2d
Masoumeh Rezai <you@example.com>
2023-04-27 22:17:12 +0430

    challenge 115

---
+++ app/challenge155.js
@@ -0,0 +1,12 @@
+function num(n) {
+    for (let i = 0; i < n; i++) {
+        if (n < 10) {
+            return false;
+        }
+        else if (n > 10) {
+            return true
+        }
+    }
+
+}
+num()
