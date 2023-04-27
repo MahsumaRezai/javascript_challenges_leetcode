4d69904ade98737f4bea5c8c4bc53059513ce453
Masoumeh Rezai <you@example.com>
2023-04-27 23:07:59 +0430

    challenge 158

---
+++ app/app.js
@@ -0,0 +1,12 @@
+function outer() {
+    let name = "Mahsoumeh";
+    let last = "Rezai";
+    function inner() {
+        console.log(name);
+        console.log(last)
+
+    }
+    inner();
+
+}
+outer()
